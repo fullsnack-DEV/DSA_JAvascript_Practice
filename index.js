a2feac20fbffea7c91f404983732b18b827d022e
Pratik Patre <56273302+fullsnack-DEV@users.noreply.github.com>
2022-02-01 08:57:42 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+//REcursion 
+//function calling itself known as Recursion 
+
+function SayName(n)  {
+
+ if(n <= 0) { 
+   return
+ }
+
+  console.log(n); 
+  SayName(n-1);
+}
+
+SayName(50)
+
+
+// if we call the above function it get stuck into infinite loop; 
+//Lets add base condition to it
